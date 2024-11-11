a69d48846ca081461983b35d5060f2e96b9cf708
Zamfirov <filip.zamfirov1996@gmail.com>
2024-11-11 12:12:01 +0100

    Rename graph module

--- M3GraphBuilder/__main__.py
+++ M3GraphBuilder/__main__.py
@@ -1,7 +1,7 @@
 import os
 import json
 import argparse
-from M3GraphBuilder.graph.validator import find_node_discrpenecies
+from M3GraphBuilder.graphlib.validator import find_node_discrpenecies
 from M3GraphBuilder.converters import Cpp
 
 
